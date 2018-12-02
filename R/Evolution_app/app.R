9c8267ff0f68b36417d04e6aa5fc0807d0e12c61
Vincent Cannataro <vincent.cannataro@yale.edu>
2018-12-01 19:30:03 -0500

    working generate population

--- R/Evolution_app/app.R
+++ R/Evolution_app/app.R
@@ -19,6 +19,7 @@ ui <- fluidPage(
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
+        
          sliderInput("cells",
                      "Number of cells:",
                      min = 1,
@@ -27,8 +28,9 @@ ui <- fluidPage(
          sliderInput("cell_size",
                      "Cell size:",
                      min = 1,
-                     max = 20,
-                     value = 5)
+                     max = 40,
+                     value = 40),
+         actionButton("pop_generate","Generate population")
       ),
 
       
@@ -39,23 +41,27 @@ ui <- fluidPage(
    )
 )
 
-# Define server logic required to draw a histogram
+# Define server logic required to draw plots and interact
 server <- function(input, output) {
    
+  observeEvent(input$pop_generate, {
+    rad <- 0.75
+    cells <- input$cells
+    mutant <- 2
+    cell_size <- input$cell_size
+    # cell_size <- 10
+    
+    population_structure_df <- data.frame(color_reps=sample(c(rep("wild type",cells-mutant),rep("mutant",mutant)),replace = F),
+                                          rate=rep(1,cells))
+    population_structure_df$angle <- seq(90,360+90,length.out = nrow(population_structure_df)+1)[1:nrow( population_structure_df)]
+    
+    population_structure_df$x_pos <- rad * cos(population_structure_df$angle*pi/180)  
+    population_structure_df$y_pos <- rad * sin(population_structure_df$angle*pi/180)  
+    # (ggplot(data = population_structure_df, aes(x=x_pos,y=y_pos)) + geom_point(aes(color=color_reps),size=cell_size) + theme_no_axes() + coord_cartesian(xlim = c(-1,1),ylim=c(-1,1)) + scale_color_discrete(name="Cell type"))
+  })
+  
    output$cellPlot <- renderPlot({
-     rad <- 0.75
-     cells <- input$cells
-     mutant <- 2
-     cell_size <- input$cell_size
-     # cell_size <- 10
-     
-     population_structure_df <- data.frame(color_reps=sample(c(rep("wild type",cells-mutant),rep("mutant",mutant)),replace = F),
-                                           rate=rep(1,cells))
-     population_structure_df$angle <- seq(90,360+90,length.out = nrow(population_structure_df)+1)[1:nrow( population_structure_df)]
-     
-     population_structure_df$x_pos <- rad * cos(population_structure_df$angle*pi/180)  
-     population_structure_df$y_pos <- rad * sin(population_structure_df$angle*pi/180)  
-     
+
      (ggplot(data = population_structure_df, aes(x=x_pos,y=y_pos)) + geom_point(aes(color=color_reps),size=cell_size) + theme_no_axes() + coord_cartesian(xlim = c(-1,1),ylim=c(-1,1)) + scale_color_discrete(name="Cell type"))
    })
 }
---
+++ R/Evolution_app/evolution_simulation_EM.R
@@ -0,0 +1,68 @@
+# 05-actionButton
+
+library(shiny)
+
+ui <- fluidPage(
+  
+  # Application title
+  titlePanel("Evolution simulation"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      
+      sliderInput("cells",
+                  "Number of cells:",
+                  min = 1,
+                  max = 50,
+                  value = 6),
+      sliderInput("cell_size",
+                  "Cell size:",
+                  min = 1,
+                  max = 40,
+                  value = 40),
+      actionButton(inputId = "gen_pop", 
+                   label = "Generate population")
+    ),
+    mainPanel(
+      plotOutput("testplot")
+              )
+  )
+  
+ 
+  # verbatimTextOutput("text")
+)
+
+server <- function(input, output) {
+  
+
+  
+  # regenerate plot
+  observeEvent(input$gen_pop, {
+    cells <- isolate({input$cells})
+    cell_size <- isolate({input$cell_size})
+    # output$text <- renderPrint({print(as.numeric(input$clicks))})
+    rad <- 0.75
+    # cells <- input$cells
+    # cells <- 6
+    mutant <- 2
+    # cell_size <- input$cell_size
+    # cell_size <- 10
+    
+    population_structure_df <- data.frame(color_reps=sample(c(rep("wild type",cells-mutant),rep("mutant",mutant)),replace = F),
+                                          rate=rep(1,cells))
+    population_structure_df$angle <- seq(90,360+90,length.out = nrow(population_structure_df)+1)[1:nrow( population_structure_df)]
+    
+    population_structure_df$x_pos <- rad * cos(population_structure_df$angle*pi/180)  
+    population_structure_df$y_pos <- rad * sin(population_structure_df$angle*pi/180)  
+    
+    output$testplot <- renderPlot({ggplot(data = population_structure_df, aes(x=x_pos,y=y_pos)) + geom_point(aes(color=color_reps),size=cell_size) + theme_no_axes() + coord_cartesian(xlim = c(-1,1),ylim=c(-1,1)) + scale_color_discrete(name="Cell type")
+    })
+  })
+  
+  
+  
+  
+}
+
+shinyApp(ui = ui, server = server)
