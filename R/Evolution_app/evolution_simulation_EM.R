0dae5217b0f6ab573cecb545ee10b307691ccee7
Vincent Cannataro <vincent.cannataro@yale.edu>
2018-12-01 22:19:46 -0500

    added fast forward button

--- R/Evolution_app/evolution_simulation_EM.R
+++ R/Evolution_app/evolution_simulation_EM.R
@@ -1,5 +1,7 @@
 library(shiny)
 
+cols <- c("wild type" = "blue", "mutant" = "red")
+
 ui <- fluidPage(
   
   # Application title
@@ -19,13 +21,20 @@ ui <- fluidPage(
                   min = 1,
                   max = 40,
                   value = 40),
+      numericInput(inputId = "mutant_rel_div",
+                   label = "Relative division rate of mutant",value = 1,min = 0),
       actionButton(inputId = "gen_pop", 
                    label = "Generate population"),
       hr(),
       
       textInput(inputId = "HT",label = "Heads or tails?",value = NULL),
       numericInput(inputId = "cell_pick",value = NULL,label = "Cell pick"),
-      actionButton(inputId = "update_pop",label = "Update population")
+      actionButton(inputId = "update_pop",label = "Update population"),
+      
+      hr(),hr(),
+      
+      actionButton(inputId = "take_it_away",label = "Fast forward")
+      
       
     ),
     
@@ -42,12 +51,13 @@ ui <- fluidPage(
 server <- function(input, output) {
   
   population_structure_df <- reactiveValues(data=NULL)
-
   
-  # (re)generate plot
- observeEvent(input$gen_pop, {
+  
+  # (re)generate data
+  observeEvent(input$gen_pop, {
     cells <- isolate({input$cells})
     cell_size <- isolate({input$cell_size})
+    relative_div_rate <- isolate({input$mutant_rel_div})
     # output$text <- renderPrint({print(as.numeric(input$clicks))})
     rad <- 0.75
     # cells <- input$cells
@@ -57,7 +67,9 @@ server <- function(input, output) {
     # cell_size <- 10
     
     population_structure_df$data <- data.frame(color_reps=sample(c(rep("wild type",cells-mutant),rep("mutant",mutant)),replace = F),
-                                          rate=rep(1,cells))
+                                               rate=1)
+    population_structure_df$data[population_structure_df$data=="mutant","rate"] <- relative_div_rate
+    
     population_structure_df$data[,"angle"] <- seq(90,360+90,length.out = nrow(population_structure_df$data)+1)[1:nrow( population_structure_df$data)]
     
     population_structure_df$data[,"x_pos"] <- rad * cos(population_structure_df$data[,"angle"]*pi/180)  
@@ -68,10 +80,10 @@ server <- function(input, output) {
         geom_point(aes(fill=color_reps),size=cell_size,shape=21,stroke=2) + 
         theme_no_axes() + 
         coord_cartesian(xlim = c(-1,1),ylim=c(-1,1)) + 
-        scale_fill_discrete(name="Cell type") + 
-        geom_text(aes(label=cell_number))
+        scale_fill_manual(name="Cell type",values=cols) + 
+        geom_text(aes(label=cell_number)) 
       
-        # return(population_structure_df)
+      # return(population_structure_df)
       
       
     })
@@ -92,13 +104,13 @@ server <- function(input, output) {
     
     if(HT=="H") {
       cell_replace <- if(cell_pick>1){cell_pick - 1}else{cells} # if value == 1, pick largest value 
-      population_structure_df$data[cell_replace,"color_reps"] <-  population_structure_df$data[cell_pick,"color_reps"] 
+      population_structure_df$data[cell_replace,c("color_reps","rate")] <-  population_structure_df$data[cell_pick,c("color_reps","rate")] 
     }
     if(HT=="T") {
       
       cell_replace <- if(cell_pick<cells){cell_pick+1}else{1}
       
-      population_structure_df$data[cell_replace,"color_reps"] <-  population_structure_df$data[cell_pick,"color_reps"] 
+      population_structure_df$data[cell_replace,c("color_reps","rate")] <-  population_structure_df$data[cell_pick,c("color_reps","rate")] 
     }
     
     arrow_start_x <- population_structure_df$data[cell_pick,"x_pos"]
@@ -118,7 +130,8 @@ server <- function(input, output) {
                      color="black",size=1,arrow = arrow(length = unit(0.3, "inches"))) + 
         theme_no_axes() + 
         coord_cartesian(xlim = c(-1,1),ylim=c(-1,1)) + 
-        scale_fill_discrete(name="Cell type")})
+        scale_fill_manual(name="Cell type",values=cols)
+      })
     
     output$test_ht <- renderDataTable({population_structure_df$data})
     
@@ -128,6 +141,55 @@ server <- function(input, output) {
   
   
   
+  
+  # fast forward 
+  observeEvent(input$take_it_away, {
+    
+    cells <- isolate({input$cells})
+    cell_size <- isolate({input$cell_size})
+    
+    # loop until extinction/fixation
+    while(length(unique(population_structure_df$data[,"color_reps"]))>1){
+      
+      cell_pick <- sample(x = 1:cells,size = 1,prob = population_structure_df$data[,"rate"])
+      HT <- sample(x = c("H","T"),size = 1)
+      
+      
+      if(HT=="H") {
+        cell_replace <- if(cell_pick>1){cell_pick - 1}else{cells} # if value == 1, pick largest value 
+        population_structure_df$data[cell_replace,c("color_reps","rate")] <-  population_structure_df$data[cell_pick,c("color_reps","rate")] 
+      }
+      if(HT=="T") {
+        
+        cell_replace <- if(cell_pick<cells){cell_pick+1}else{1}
+        
+        population_structure_df$data[cell_replace,c("color_reps","rate")] <-  population_structure_df$data[cell_pick,c("color_reps","rate")] 
+      }
+      
+      arrow_start_x <- population_structure_df$data[cell_pick,"x_pos"]
+      arrow_end_x <- population_structure_df$data[cell_replace,"x_pos"]
+      arrow_start_y <- population_structure_df$data[cell_pick,"y_pos"]
+      arrow_end_y <- population_structure_df$data[cell_replace,"y_pos"]
+      
+      output$testplot <- renderPlot({ggplot(data = population_structure_df$data, aes(x=x_pos,y=y_pos)) + 
+          geom_point(aes(fill=color_reps),size=cell_size,shape=21,stroke=2) + 
+          geom_point(data=population_structure_df$data[c(cell_pick,cell_replace),],
+                     aes(x=x_pos,y=y_pos,fill=color_reps),
+                     color="yellow",shape=21,stroke=2,size=cell_size) + 
+          geom_segment(aes(x=arrow_start_x,
+                           xend=arrow_end_x,
+                           y=arrow_start_y,
+                           yend=arrow_end_y),
+                       color="black",size=1,arrow = arrow(length = unit(0.3, "inches"))) + 
+          theme_no_axes() + 
+          coord_cartesian(xlim = c(-1,1),ylim=c(-1,1)) + 
+          scale_fill_manual(name="Cell type",values=cols)
+        
+        })
+      
+    }
+    
+  })
   
 }
 
